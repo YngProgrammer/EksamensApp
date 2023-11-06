e95845d7f54903332d732c47989f876d3f12ba76
Kimsalabim0130 <91661298+Kimsalabim0130@users.noreply.github.com>
2023-11-06 16:39:09 +0100

    Create Payment.java

---
+++ EksamensApp/target/classes/com/desktopAppExamen/Payment.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ * File: Payment.java
+ * Description: This is an entity class for "Payment" and encapsulates the details of a payment, including the date of payment and the amount.
+ * The payment data comes from 3rd-party. 
+ * @author Kim
+ * @version 06.11.2023
+*/ 
+
+
+package com.desktopAppExamen.Payments;
+
+public class Payment {
+    private String checkNr;
+    private date paymentDate;
+    private decimal amount;
+
+    public Payment(String checkNr, date paymentDate, decimal amount){
+        this.checkNr = checkNr;
+        this.paymentDate = paymentDate;
+        this.amount = amount;
+    }
+    public String getcheckNr(){
+        return checkNr;
+    }
+    public date paymentDate(){
+        return paymentDate;
+    }
+    public decimal amount(){
+        return amount;
+    }
+    public void setCheckNr(String checkNr){
+        this.checkNR = checkNr;
+    }
+    public void setPaymentDate(Date paymentDate){
+        this.paymentDate = paymentDate;
+    }
+    public void setAmount(decimal amount){
+        this.amount = amount;
+    }
+}
