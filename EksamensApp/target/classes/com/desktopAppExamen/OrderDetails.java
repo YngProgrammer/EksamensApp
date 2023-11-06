703f1e34a21b048f5b3e24abec1b42ba8ed567d1
Kimsalabim0130 <91661298+Kimsalabim0130@users.noreply.github.com>
2023-11-06 16:40:45 +0100

    Create OrderDetails.java

---
+++ EksamensApp/target/classes/com/desktopAppExamen/OrderDetails.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ * File: OrderDetails.java
+ * Description: This is a "orderDetails" class and encapsulates the details of an order, including order quantities and the unit price. 
+ * @author Kim
+ * @version 06.11.2023
+*/ 
+
+package package com.desktopAppExamen.OrderDetails;
+
+
+public class OrderDetails implements Entity {
+   private int quantityOrdered;
+   private double priceEach;
+   private int orderLineNr;
+
+   public OrderDetails(int quantityOrdered, double priceEach) {
+      this.quantityOrdered = quantityOrdered;
+      this.priceEach = priceEach;
+   }
+
+   public int getQuantityOrdered() {
+      return this.quantityOrdered;
+   }
+
+   public double getPriceEach() {
+      return this.priceEach;
+   }
+
+   public double calculateTotalPrice(int quantityOrdered, double priceEach) {
+      return quantityOrdered * priceEach;
+   }
+}
