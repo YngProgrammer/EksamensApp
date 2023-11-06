19114482472c0b9595500943c8eff79caa221423
AlbertAsmervik <Albertasmervik@gmail.com>
2023-11-06 16:30:57 +0100

    Update ForslagTilMal.java

--- EksamensApp/src/com/desktopAppExamen/mulig/ForslagTilMal.java
+++ EksamensApp/src/com/desktopAppExamen/mulig/ForslagTilMal.java
@@ -17,7 +17,7 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.event.*;
 import com.desktopAppExamen.connection.DataBaseConnection; // Import the database connection class
 
-public class Payment {
+public class ForslagTilMal {
     private String checkNr;
     private Date paymentDate;
     private BigDecimal amount;
@@ -29,7 +29,7 @@ public class Payment {
      * @param paymentDate The payment date.
      * @param amount      The payment amount.
      */
-    public Payment(String checkNr, Date paymentDate, BigDecimal amount) {
+    public ForslagTilMal(String checkNr, Date paymentDate, BigDecimal amount) {
         this.checkNr = checkNr;
         this.paymentDate = paymentDate;
         this.amount = amount;
