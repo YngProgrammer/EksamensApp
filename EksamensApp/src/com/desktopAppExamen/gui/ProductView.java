f3d1d53cc819474d9200ea3e0c899afe3e5cf9b5
OleSun <127397159+OleSun@users.noreply.github.com>
2023-11-07 15:03:41 +0100

    Create ProductView.java
    Har laget et enkelt rammeverk for GUI. Greit å ta utgangspunkt i.

---
+++ EksamensApp/src/com/desktopAppExamen/gui/ProductView.java
@@ -0,0 +1,71 @@
+package com.desktopAppExamen.mulig.Entiteter;
+
+import javax.swing.*;
+
+import com.desktopAppExamen.Products.Products;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ProductsView {
+    private JFrame frame;
+    private DefaultListModel<Products> productListModel;
+    private JList<Products> productList;
+    private JButton addButton;
+    private JButton removeButton;
+
+    public ProductsView() {
+        frame = new JFrame("Product Management");
+        productListModel = new DefaultListModel<>();
+        productList = new JList<>(productListModel);
+        addButton = new JButton("Add Product");
+        removeButton = new JButton("Remove Product");
+
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Implement the logic to add a product to the list
+            }
+        });
+
+        removeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Implement the logic to remove a product from the list
+            }
+        });
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        panel.add(productList, BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout());
+        buttonPanel.add(addButton);
+        buttonPanel.add(removeButton);
+        panel.add(buttonPanel, BorderLayout.SOUTH);
+
+        frame.getContentPane().add(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setVisible(true);
+    }
+
+    public void addProduct(Products product) {
+        productListModel.addElement(product);
+    }
+
+    public void removeProduct(Products product) {
+        productListModel.removeElement(product);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ProductsView();
+            }
+        });
+    }
+}
