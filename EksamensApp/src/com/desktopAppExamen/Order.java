c7ddae5b062690a2d6c6fb3c91ebffbc8f57ba65
Kimsalabim0130 <91661298+Kimsalabim0130@users.noreply.github.com>
2023-11-07 12:34:40 +0100

    Update Order.java

--- EksamensApp/src/com/desktopAppExamen/Order.java
+++ EksamensApp/src/com/desktopAppExamen/Order.java
@@ -2,29 +2,47 @@
  * 
  * File: Order.java
  * Description: This is an entity class for "order" and encapsulates the basic information of an order.
- * This class contains information about delivery status as well. 
+ * This class contains information about delivery status as well.
+ * This class contains a calculateOrderTotal() method that returns the total amount of the order. 
  * @author Kim
  * @version 06.11.2023
 */ 
 
-package package com.desktopAppExamen.Order;
+package com.desktopAppExamen.Order;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Order implements Entity {
-   private int orderNr;
+
+   //Declaration of private data fields 
+
+   private final int orderNr;
    private Date orderDate;
    private Date requiredDate;
    private Date shippedDate;
    private String status;
    private String comments;
+   private List<OrderDetails> orderDetailsList;
+
+   /**
+   * Constructor for Order class.
+   * @param orderNr        The unique serial number consists of 5 digits.
+   */
 
    public Order(int orderNr) {
       this.orderNr = orderNr;
+      this.orderDetailsList = new ArrayList<>();
    }
 
+   /**
+   * Getter methods for following variables.
+   * @return order number, order date, required date, shipped date, delivery status, and comments.
+   */
+
    public int getOrderNumber() {
-      return this.orderNumber;
+      return this.orderNr;
    }
 
    public Date getOrderDate() {
@@ -46,5 +64,53 @@ public class Order implements Entity {
    public String getComments() {
       return this.comments;
    }
+
+   /**
+   * Setter methods for following parameters.
+   * setting @param orderDate, @param requiredDate, @param shippedDate, @param status, @param comments   
+   */
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public void setRequiredDate(Date requiredDate) {
+        this.requiredDate = requiredDate;
+    }
+
+    public void setShippedDate(Date shippedDate) {
+        this.shippedDate = shippedDate;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    /**
+    * Add 'OrderDetails' object to the list of order details associated with this order
+    */
+    public void addOrderDetail(OrderDetails orderDetail){
+        orderDetailsList.add(orderDetail);
+    }
+
+    /**
+    * Calculation the thotal cost of the entire order by summing up the subtotals of each order details
+    * @return order total     
+    */
+
+    public double calculateOrderTotal() {
+    double total = 0.0;
+
+    for (orderDetails orderDetail : orderDetailsList) {
+        double subtotal = orderDetail.calculateSubTotal();
+        total += subtotal;
+    }
+
+    return total;
+    }
 
 }
