3683c82e82c486ca7e18be7c6dbed1f53470fa4b
Kimsalabim0130 <91661298+Kimsalabim0130@users.noreply.github.com>
2023-11-07 13:12:55 +0100

    Update OrderDetails.java

--- EksamensApp/src/com/desktopAppExamen/OrderDetails.java
+++ EksamensApp/src/com/desktopAppExamen/OrderDetails.java
@@ -1,22 +1,39 @@
 /**
  * 
  * File: OrderDetails.java
- * Description: This is a "orderDetails" class and encapsulates the details of an order, including order quantities and the unit price. 
+ * Description: This is a class for order details and encapsulates the details of an order, including order quantities and the unit price. 
+ * This class contains a cunstructor, and getter & setter methods.
+ * This class contains a calculateSubTotal() method that returns sub total price of an order.
  * @author Kim
- * @version 06.11.2023
+ * @version 07.11.2023
 */ 
 
-package package com.desktopAppExamen.OrderDetails;
+package com.desktopAppExamen.OrderDetails;
 
 public class OrderDetails implements Entity {
+   
+   //Declaration of private data fields 
    private int quantityOrdered;
    private double priceEach;
    private int orderLineNr;
 
-   public OrderDetails(int quantityOrdered, double priceEach) {
+   /**
+   * Constructor for OrderDetails class.
+   * @param quantityOrdered    The quantity of the product ordered.
+   * @param priceEach          The unit price of the product.
+   * @param orderLineNr        The order line number.
+   */
+
+   public OrderDetails(int quantityOrdered, double priceEach, int orderLineNr) {
       this.quantityOrdered = quantityOrdered;
       this.priceEach = priceEach;
+      this.orderLineNr = orderLineNr;
    }
+
+   /**
+   * Getter methods for product quantity, unit price, and the order line number.
+   * @return q'ty, unit price, order line number.
+   */
 
    public int getQuantityOrdered() {
       return this.quantityOrdered;
@@ -26,14 +43,31 @@ public class OrderDetails implements Entity {
       return this.priceEach;
    }
 
-    public void setQuantityOrdered(int quantityOrdered){
+   public int getorderLineNr() {
+      return this.orderLineNr;
+   }
+
+   /**
+   * Setter methods for product quantity, unit price, and the order line number.
+   * setting @param quantityOrdered, @param priceEach, @param orderLineNr   
+   */
+
+   public void setQuantityOrdered(int quantityOrdered){
         this.quantityOrdered = quantityOrdered;
     }
-    public void setPriceEach(double priceEach){
-        this.paymentDate = paymentDate;
+   public void setPriceEach(double priceEach){
+        this.priceEach = priceEach;
+    }
+   public void setOrderLineNr(int orderLineNr){
+        this.orderLineNr = orderLineNr;
     }
 
-   public double calculateTotalPrice(int quantityOrdered, double priceEach) {
+   /**
+   * Calculation methods for a sub total amount
+   * @return subtotal     
+   */
+
+   public double calculateSubTotal() {
       return quantityOrdered * priceEach;
    }
 }
