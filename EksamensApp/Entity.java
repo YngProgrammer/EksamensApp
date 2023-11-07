e0c055dc18d10f19ef32d1a01a53c6223310e48e
OleSun <127397159+OleSun@users.noreply.github.com>
2023-11-07 15:18:26 +0100

    Create Entity.java
    Abstract class:
    
     Description: This is an abstract base class. Even though currently empty, it makes it easier to add new entities or common attributes/methods to the ERP-system at a later stage.

---
+++ EksamensApp/Entity.java
@@ -0,0 +1,12 @@
+  /**
+ * Abstract Class: Entity
+ * Description: This is an abstract base class. Even though currently empty, it makes it easier to add new entities or common attributes/methods to the ERP-system at a later stage.
+ * Author: Ole
+ * Version: 7.11.2023
+ */
+
+public class Entity {
+
+
+}
+
